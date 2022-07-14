5116851d7c1afcfebb01ef8a5fc987942b222a7e
Harry-qi <30359281+Harry-qi@users.noreply.github.com>
2022-07-14 04:37:09 +0000

    Deploying to gh-pages from @ Harry-qi/blog@7d8a7e288c4f8f294b037431edd5daa439052934 🚀

---
+++ js/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?3edcded5786cf5c7725bb8833c4a5028";
+    var s = document.getElementsByTagName("script")[0]; 
+    s.parentNode.insertBefore(hm, s);
+})();
